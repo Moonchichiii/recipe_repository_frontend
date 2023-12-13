e5b52bf04b07cee06d6a7c7a7e55ad6dd766f937
Moonchichiii <>
2023-12-13 10:23:42 +0000

    secure start

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from './components/common/Navbar/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+
+  
+
+    <main>
+      <div>
+      <Navbar />
+
+        <h1>Hello!!!!</h1>
+
+       </div>
+       </main>
+    </>
+  )
+};
+
+export default App
